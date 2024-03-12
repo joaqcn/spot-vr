308920685529fa836e43df98e9846936a96f96a5
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2024-03-11 22:44:08 -0400

    Movement completed

--- Reu/Assets/Controls/ModelMovement.cs
+++ Reu/Assets/Controls/ModelMovement.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Robotics;
 
 public class ModelMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private ArticulationBody rootBody;
 
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float rotationSpeed = 1f;
-    [SerializeField] private Rigidbody rb;
-    
-    
-    // Update is called once per frame
     void Update()
     {
-        var velocity = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")* speed);
-        velocity.y = rb.velocity.y;
-        rb.velocity = velocity;
-        
-        
+        MoveModel();
+    }
 
+    private void MoveModel()
+    {
+        Vector3 movement = Vector3.zero;
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += Vector3.back;
+        }
+
+        // Calculate the new position based on the input and speed
+        Vector3 newPosition = rootBody.transform.position + movement * (speed * Time.deltaTime);
+
+        // Teleport the root to the new position
+        TeleportRoot(newPosition, rootBody.transform.rotation);
+    }
+
+    // Your existing TeleportRoot function
+    private void TeleportRoot(Vector3 position, Quaternion rotation)
+    {
+        rootBody.TeleportRoot(position, rotation);
     }
 }
