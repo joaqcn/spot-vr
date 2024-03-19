797defa430a5b1cdda74be13dfde1c98a4c39354
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2024-03-19 00:29:15 -0400

    Added left and right animation, needs to fix direction

--- Reu/Assets/Controls/ModelMovement.cs
+++ Reu/Assets/Controls/ModelMovement.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Robotics;
-using UnityEngine.UIElements;
 
 public class ModelMovement : MonoBehaviour
 {
+    [SerializeField] private GameObject modelGameObject;
     [SerializeField] private float speed = 2f;
     [SerializeField] private float rotationSpeed = 100f;
     [SerializeField] private ArticulationBody rootBody;
     [SerializeField] private GameObject animatorGameObject; // Reference to the GameObject with the animator component
     private Animator animator;
-
 
     private void Start()
     {
@@ -27,37 +25,37 @@ public class ModelMovement : MonoBehaviour
 
         animator.speed = 0;
     }
+
     void Update()
     {
         MoveModel();
-       
     }
 
     private void MoveModel()
     {
         Vector3 movement = Vector3.zero;
-        
+
+        // Calculate movement based on input
         movement = Movement(movement);
-        
+
         // Calculate the new position based on the input and speed
         Vector3 newPosition = rootBody.transform.position + movement * (speed * Time.deltaTime);
 
         // Teleport the root to the new position
         rootBody.TeleportRoot(newPosition, rootBody.transform.rotation);
-       
+
+        // Rotate the GameObject based on input
+        RotateModel();
     }
 
     private Vector3 Movement(Vector3 movement)
     {
-        
-        Vector3 newPosition = rootBody.transform.position + movement * (speed * Time.deltaTime);
-        
         if (Input.GetKey(KeyCode.A))
         {
             movement += Vector3.left;
             animator.speed = 1;
         }
-        else if (Input.GetKeyUp(KeyCode.A))
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
         {
             animator.speed = 0;
         }
@@ -67,10 +65,6 @@ public class ModelMovement : MonoBehaviour
             movement += Vector3.right;
             animator.speed = 1;
         }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            animator.speed = 0;
-        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -78,49 +72,26 @@ public class ModelMovement : MonoBehaviour
             animator.speed = 1;
         }
 
-        else if (Input.GetKeyUp(KeyCode.W))
-        {
-            animator.speed = 0;
-        }
-
         if (Input.GetKey(KeyCode.S))
         {
             movement += Vector3.back;
             animator.speed = 1;
         }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            animator.speed = 0;
-        }
 
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            transform.Rotate(-Vector3.up * (rotationSpeed * Time.deltaTime));
-            animator.speed = 1;
-        }
-        else if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            animator.speed = 0;
-        }
-        
-        if (Input.GetKey(KeyCode.Mouse1))
-        {
-            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-
-                animator.speed = 1;
-        }
-        else if (Input.GetKeyUp(KeyCode.Mouse1))
-        {
-            animator.speed = 0;
-        }
         return movement;
     }
-  
 
-    // Your existing TeleportRoot function
-    private void TeleportRoot(Vector3 position, Quaternion rotation)
+    private void RotateModel()
     {
-        rootBody.TeleportRoot(position, rotation);
+        // Rotate the modelGameObject based on input
+        if (Input.GetKey(KeyCode.Q))
+        {
+            modelGameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            modelGameObject.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
     }
 }
-    
